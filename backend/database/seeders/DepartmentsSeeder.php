6c1ad0ee3f642b86954a01df0beee6605503c9e1
xKurty06 <zeankurtbalboa06@gmail.com>
2026-05-16 23:25:35 +0800

    feat: Update DepartmentsSeeder with additional departments and corrected codes

--- backend/database/seeders/DepartmentsSeeder.php
+++ backend/database/seeders/DepartmentsSeeder.php
@@ -10,14 +10,17 @@ class DepartmentsSeeder extends Seeder
     public function run(): void
     {
         $departments = [
+            ['name' => 'College of Engineering and Information Technology', 'code' => 'CEIT'],
+            ['name' => 'College of Economics, Management and Development Studies', 'code' => 'CEMDS'],
             ['name' => 'College of Arts and Sciences', 'code' => 'CAS'],
-            ['name' => 'College of Engineering', 'code' => 'CE'],
-            ['name' => 'College of Computer Studies', 'code' => 'CCS'],
-            ['name' => 'College of Business and Entrepreneurship', 'code' => 'CBE'],
-            ['name' => 'College of Education', 'code' => 'CEduc'],
-            ['name' => 'College of Nursing', 'code' => 'CN'],
+            ['name' => 'College of Nursing', 'code' => 'CON'],
+            ['name' => 'College of Veterinary Medicine and Biomedical Sciences', 'code' => 'CVMBS'],
+            ['name' => 'College of Tourism and Hospitality Management', 'code' => 'CTHM'],
             ['name' => 'College of Criminal Justice', 'code' => 'CCJ'],
-            ['name' => 'College of Agriculture, Food Technology, and Nutrition', 'code' => 'CAFTN'],
+            ['name' => 'College of Sports, Physical Education and Recreation', 'code' => 'CSPEAR'],
+            ['name' => 'College of Agriculture, Food, Environment, and Natural Resources', 'code' => 'CAFENR'],
+            ['name' => 'College of Education', 'code' => 'CED'],
+            ['name' => 'College of Medicine', 'code' => 'COM'],
         ];
 
         foreach ($departments as $d) {
